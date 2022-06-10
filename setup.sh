625093d9f565084c200b6562a7713d8beda63dff
Dominic van Berkel <dominic@baudvine.net>
2022-06-10 17:22:59 +0200

    automate everything between clone and upload

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+PY=${PY:-python}
+
+# Top-level submodules
+git submodule update --init
+
+cd pelican-plugins
+# Submodules for specific plugins in pelican-plugins
+git submodule update --init pelican-open_graph
+cd ..
+
+$PY -m pip install --user pipenv
+$PY -m pipenv install
+$PY -m pipenv run make publish
